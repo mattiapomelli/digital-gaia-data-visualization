8bc0fd54da1ee62b51d13341d52cdb632a04d85a
mattiapomelli <mattiapomelli@gmail.com>
2023-05-10 14:18:43 +0100

    Add api route to generate analysis with gpt

---
+++ pages/api/generate.js
@@ -0,0 +1,49 @@
+const { Configuration, OpenAIApi } = require("openai");
+
+import reportData from "../../data/report.json";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const basePromptPrefix = `
+Given this dataset about a hemp farm.
+
+Dataset:
+`;
+
+export default async function handler(req, res) {
+  if (req.method === "GET") {
+    const query = req.query.query;
+
+    const observations = reportData.map(({ observations }) => ({
+      observations,
+    }));
+
+    const prompt = `
+      ${basePromptPrefix}${JSON.stringify(observations)}
+      
+
+      ${query}
+      `;
+
+    try {
+      const completion = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt,
+        temperature: 0.7,
+        max_tokens: 500,
+      });
+
+      const output = completion.data.choices[0].text;
+
+      return res.status(200).json(output);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: "Internal server error" });
+    }
+  } else {
+    return res.status(405).json({ message: "Method is not allowed" });
+  }
+}
